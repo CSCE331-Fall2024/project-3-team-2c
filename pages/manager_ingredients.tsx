9c101795de626d4b581ef28ed331a7e9e95a3259
Jonathan Herrera <Jonathan.a.herrera0@gmail.com>
2024-10-29 12:34:16 -0500

    initial shadcn installation

---
+++ pages/manager_ingredients.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from 'src/components/ui/navigation-menu';
+
+const ManagerIngredients: React.FC = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuIndicator />
+      <NavigationMenuViewport />
+    </NavigationMenu>
+  );
+};
+
+export default ManagerIngredients;
