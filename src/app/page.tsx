43ae6961ff2ff027f8f70487fc941292570d227b
Vincent Nguyen <Vincentnguyen030@gmail.com>
2024-10-29 17:06:45 -0500

    Add new main menu page to route to other pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ManagerMenu from "./_components/Manager/managerMenu";
+import MainMenu from "./_components/Main/mainMenu";
 import "./page.css";
 
 export default function Home() {
-  return <ManagerMenu />;
+  return <MainMenu />;
 }
