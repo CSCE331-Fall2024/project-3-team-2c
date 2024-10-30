dca37e772213457f43b8ac1ad5c5e8bdaf8a40f2
ericliu-12 <133845008+alaska-summer@users.noreply.github.com>
2024-10-30 17:33:01 -0500

    Fix merge conflicts

--- pages/Main/mainMenu.tsx
+++ pages/Main/mainMenu.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-// import './mainMenu.css';
+import React from "react";
+import "./mainMenu.css";
 
 export default function MainMenu() {
-    return (
-        <div className="main-menu">
-            <header className="logo-container">
-                PANDA EXPRESS
-            </header>
-            <div className="panel-grid">
-                <div className="panel manager">
-                    <span className="panel-text">Manager</span>
-                </div>
-                <div className="panel cashier">
-                    <span className="panel-text">Cashier</span>
-                </div>
-                <div className="panel customer">
-                    <span className="panel-text">Customer</span>
-                </div>
-                <div className="panel menu-board">
-                    <span className="panel-text">Menu Board</span>
-                </div>
-            </div>
+  return (
+    <div className="main-menu">
+      <header className="logo-container">PANDA EXPRESS</header>
+      <div className="panel-grid">
+        <div className="panel manager">
+          <span className="panel-text">Manager</span>
         </div>
-    );
+        <div className="panel cashier">
+          <span className="panel-text">Cashier</span>
+        </div>
+        <div className="panel customer">
+          <span className="panel-text">Customer</span>
+        </div>
+        <div className="panel menu-board">
+          <span className="panel-text">Menu Board</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/Manager/managerMenu.tsx
+++ pages/Manager/managerMenu.tsx
@@ -1,40 +1,46 @@
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-} from "~/components/ui/navigation-menu"
-// import "./managerMenu.css"; 
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "~/components/ui/navigation-menu";
+import "./managerMenu.css";
 
 export default function ManagerMenu() {
-    return (
-        <main className="managermenu">
-            <h1>
-                Manager Menu
-            </h1>
-            <NavigationMenu>
-                <NavigationMenuList className="horizontal-tabs">
-                    {/* Tab for Employee */}
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="menulink" href="/employees">Employees</NavigationMenuLink>
-                    </NavigationMenuItem>
+  return (
+    <main className="managermenu">
+      <h1>Manager Menu</h1>
+      <NavigationMenu>
+        <NavigationMenuList className="horizontal-tabs">
+          {/* Tab for Employee */}
+          <NavigationMenuItem>
+            <NavigationMenuLink className="menulink" href="/employees">
+              Employees
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-                    {/* Tab for Menu Items */}
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="menulink" href="/menu-items">Menu Items</NavigationMenuLink>
-                    </NavigationMenuItem>
+          {/* Tab for Menu Items */}
+          <NavigationMenuItem>
+            <NavigationMenuLink className="menulink" href="/menu-items">
+              Menu Items
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-                    {/* Tab for Inventory */}
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="menulink" href="/inventory">Inventory</NavigationMenuLink>
-                    </NavigationMenuItem>
+          {/* Tab for Inventory */}
+          <NavigationMenuItem>
+            <NavigationMenuLink className="menulink" href="/inventory">
+              Inventory
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-                    {/* Tab for Records */}
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="menulink" href="/records">Records</NavigationMenuLink>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-        </main>
-    );
+          {/* Tab for Records */}
+          <NavigationMenuItem>
+            <NavigationMenuLink className="menulink" href="/records">
+              Records
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </main>
+  );
 }
