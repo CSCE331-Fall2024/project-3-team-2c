e7ee8720840d0e919af7c3b3a2931bac623372bb
Vincent Nguyen <Vincentnguyen030@gmail.com>
2024-11-11 16:34:05 -0600

    Change main routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
-"use client";
-import { useEffect } from "react";
+// "use client";
+// import { useEffect } from "react";
 
-export default async function Home() {
-  useEffect(() => {
-    const fetchIngredients = async () => {
-      try {
-        const response = await fetch("/api/ingredients");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        console.log("Ingredients:", data);
-      } catch (error) {
-        console.error("Error fetching ingredients:", error);
-      }
-    };
+// export default async function Home() {
+//   useEffect(() => {
+//     const fetchIngredients = async () => {
+//       try {
+//         const response = await fetch("/api/ingredients");
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+//         const data = await response.json();
+//         console.log("Ingredients:", data);
+//       } catch (error) {
+//         console.error("Error fetching ingredients:", error);
+//       }
+//     };
 
-    fetchIngredients();
-  }, []);
+//     fetchIngredients();
+//   }, []);
 
-  return <div>Check the console for ingredients data.</div>;
+//   return <div>Check the console for ingredients data.</div>;
+// }
+
+import MainMenu from "./MainMenu/page";
+
+export default function Home() {
+  return (
+    <MainMenu />
+  );
 }
