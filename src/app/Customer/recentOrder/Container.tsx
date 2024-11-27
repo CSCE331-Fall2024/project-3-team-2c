5df41171224f2f1d4944560323883f9444c6dd32
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-11-26 20:00:56 -0600

    Added employee CRUD

--- src/app/Customer/recentOrder/Container.tsx
+++ src/app/Customer/recentOrder/Container.tsx
@@ -1,34 +1,39 @@
 "use client";
-import { api } from "~/trpc/react"
+import { api } from "~/trpc/react";
 
-export default function Container({mainList, sideList, sizeId}: {mainList: number[], sideList: number[], sizeId: number}){
-    const temp = api.menu.getMenuItemsByIds.useQuery( mainList )
-    const main = temp.data!
-    const temp2 = api.menu.getMenuItemsByIds.useQuery( sideList )
-    const side = temp2.data!
-    const temp3 = api.container.getSizeFromId.useQuery( sizeId )
-    const sizeType = temp3.data!
-    return(
-        <div>
-            <div className="flex justify-between items-center">
-                <p className="text-lg font-bold">
-                    {sizeType?.name ? sizeType.name : "none"}
-                </p>
-                {/*<p className="text-right">${item.price.toFixed(2)}</p>*/}
-            </div>
-            { (
-                
-                <ul className="ml-4 mt-1 text-sm text-gray-500 list-disc">
-                    {main?.map((subItem, subIndex) => (
-                        <li key={subIndex}>{subItem.name ?? "none"}</li>
-                    ))}
-                    {side?.map((subItem, subIndex) => (
-                        <li key={subIndex}>{subItem.name ?? "none"}</li>
-                    ))}
-                </ul>
-                
-                
-            )}
-        </div>  
-    );
+export default function Container({
+  mainList,
+  sideList,
+  sizeId,
+}: {
+  mainList: number[];
+  sideList: number[];
+  sizeId: number;
+}) {
+  const temp = api.menu.getMenuItemsByIds.useQuery(mainList);
+  const main = temp.data!;
+  const temp2 = api.menu.getMenuItemsByIds.useQuery(sideList);
+  const side = temp2.data!;
+  const temp3 = api.containers.getSizeFromId.useQuery(sizeId);
+  const sizeType = temp3.data!;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <p className="text-lg font-bold">
+          {sizeType?.name ? sizeType.name : "none"}
+        </p>
+        {/*<p className="text-right">${item.price.toFixed(2)}</p>*/}
+      </div>
+      {
+        <ul className="ml-4 mt-1 list-disc text-sm text-gray-500">
+          {main?.map((subItem, subIndex) => (
+            <li key={subIndex}>{subItem.name ?? "none"}</li>
+          ))}
+          {side?.map((subItem, subIndex) => (
+            <li key={subIndex}>{subItem.name ?? "none"}</li>
+          ))}
+        </ul>
+      }
+    </div>
+  );
 }
---
+++ src/server/api/routers/employees.ts
@@ -0,0 +1,77 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import { employees } from "~/server/db/schema";
+import { db } from "~/server/db";
+import { eq } from "drizzle-orm";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+async function getOneEmployee(input: number) {
+  console.log("getOneEmployee", input);
+  return (await db.select().from(employees).where(eq(employees.id, input)))?.at(
+    0,
+  );
+}
+
+const outputSchema = createSelectSchema(employees);
+const insertSchema = createInsertSchema(employees);
+const updateSchema = z.object({
+  ...insertSchema.shape,
+  id: z.number(),
+});
+
+export const employeesRouter = createTRPCRouter({
+  getEmployeeById: publicProcedure
+    .input(z.number())
+    .output(outputSchema)
+    .query(async ({ input }) => {
+      return (await getOneEmployee(input))!;
+    }),
+
+  getAllEmployees: publicProcedure.output(z.array(outputSchema)).query(() => {
+    return db.select().from(employees);
+  }),
+
+  getEmployeesByIds: publicProcedure
+    .input(z.array(z.number()))
+    .output(z.array(outputSchema))
+    .query(async ({ input }) => {
+      console.log("running getOneEmployee Query", input.length);
+      if (input.length == 0) {
+        return [];
+      }
+      return Promise.all(
+        input.map(async (id) => {
+          console.log(id);
+          return (await getOneEmployee(id))!;
+        }),
+      );
+    }),
+
+  getEmployeesByRole: publicProcedure
+    .input(z.string())
+    .output(z.array(outputSchema))
+    .query(async ({ input }) => {
+      return db
+        .select()
+        .from(employees)
+        .where(eq(employees.role, input.toUpperCase()));
+    }),
+
+  addMenuItem: publicProcedure
+    .input(insertSchema)
+    .mutation(async ({ input }) => {
+      return db.insert(employees).values(input);
+    }),
+
+  updateMenuItem: publicProcedure
+    .input(updateSchema)
+    .mutation(async ({ input }) => {
+      return db.update(employees).set(input).where(eq(employees.id, input.id));
+    }),
+
+  deleteMenuItem: publicProcedure
+    .input(z.number())
+    .mutation(async ({ input }) => {
+      return db.delete(employees).where(eq(employees.id, input));
+    }),
+});
