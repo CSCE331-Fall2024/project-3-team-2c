f12af068fe31cdcd4576338903ddf482acaa1aed
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-12-07 01:51:25 -0600

    Added auth checks to cashier and manager

--- src/app/manager/layout.tsx
+++ src/app/manager/layout.tsx
@@ -1,18 +1,18 @@
 import "~/styles/globals.css";
-import { auth } from "~/server/auth";
+import { auth, signIn } from "~/server/auth";
 import { redirect } from "next/navigation";
 
-export default async function CashierAuthExampleLayout({
+export default async function ManagerAuthExampleLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const session = await auth();
 
   if (!session) {
-    redirect("/");
+    await signIn();
   }
 
-  if (session?.user?.role !== "cashier" && session?.user?.role !== "manager") {
-    redirect("/")
+  if (session?.user?.role !== "manager") {
+    redirect("/");
   }
 
   return <div>{children}</div>;
